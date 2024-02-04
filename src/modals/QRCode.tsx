cd08c3fc323b128a436188efaa957685100c9353
TheKelp1e <vova_krasnov_2004@mail.ru>
2024-02-04 17:26:10 +0300

    qr code downloader

--- src/modals/QRCode.tsx
+++ src/modals/QRCode.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
-import QRCodeLib from 'qrcode.react';
+import React, { useRef } from "react";
+import QRCodeLib from "qrcode.react";
+import domtoimage from "dom-to-image";
+import { saveAs } from "file-saver";
 
 interface QRCodeProps {
   data: string;
 }
 
 const QRCode: React.FC<QRCodeProps> = ({ data }) => {
+  const qrCodeRef = useRef<HTMLDivElement | null>(null);
+
+  const downloadQRCode = () => {
+    if (qrCodeRef.current) {
+      domtoimage
+        .toBlob(qrCodeRef.current)
+        .then((blob: Blob | null) => {
+          if (blob) {
+            saveAs(blob, "qrcode.png");
+          }
+        })
+        .catch((error: Error) => {
+          console.error("Error generating QR code image:", error);
+        });
+    }
+  };
+
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-      <QRCodeLib value={data} size={256} />
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <div ref={qrCodeRef} style={{ marginBottom: "10px" }}>
+        <QRCodeLib value={data} size={256} />
+      </div>
+      <button className="btn btn-primary" onClick={downloadQRCode}>
+        Download QR Code
+      </button>
     </div>
   );
 };
