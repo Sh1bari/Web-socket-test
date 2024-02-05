83d2862bda561d3f3aba8678455df1359d0b8c19
Sh1bari <vova_krasnov_2004@mail.ru>
2024-02-06 02:38:20 +0300

    fix

--- src/components/WebSocketComponent.tsx
+++ src/components/WebSocketComponent.tsx
@@ -10,7 +10,7 @@ const WebSocketComponent: React.FC = () => {
   const [receivedMessages, setReceivedMessages] = useState<string[]>([]);
   const [stompClient, setStompClient] = useState<Stomp.Client | null>(null);
 
-  const socketUrl = 'http://localhost:8082/api/ws'; // Замените на ваш URL сервера WebSocket
+  const socketUrl = 'http://35.195.246.213/api/ws'; // Замените на ваш URL сервера WebSocket
 
   const headers = {
     Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMCIsInN1YlR5cGUiOiJkZXZpY2UiLCJ0b2tlblR5cGUiOiJhY2Nlc3MtdG9rZW4iLCJkZXZpY2VJZCI6IjQiLCJpYXQiOjE3MDY3MDg2OTR9.r5SYCh5PuO4Dt2MZuh3vG2SHtqfGQsYdxGkfVSjm0CE` // Замените на ваш токен аутентификации
